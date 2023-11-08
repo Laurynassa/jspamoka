0684fc549e90370f00715bb1bfe2ccaf3e6c16b9
Moksleivis <Moksleivis@kitm.local>
2023-11-08 13:43:26 +0200

    wasd

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+console.log("hello world!");
+
+var name = "Petras";
+console.log(name);
+var name = "Jonas";
+console.log(name);
+let name1 = "Tomas";
+console.log(name1);
+name1 = "Lokys";
+console.log(name1);
+const name2 = "Pirmas";
+console.log(name2);
+// name3 = "Antras";
+// console.log(name3);
+let first;
+first = "Car";
+console.log(first);
+// const second;
+// second = "Pukas";
+// console.log(second);
+const language = "js";
+const author = "brendam eich";
+console.log(language + " was vreated by " + author);
+const age = 25;
+const price = 11.99;
+console.log(2 + 3); // 5
+console.log(7 / 2); // 3.5
+console.log("Ejo " + name2 + " ir sutiko " + name1 + " Pabaiga");
+console.log(`Ejo ${name2} ir sutiko ${name1} pabaiga `);
+let mystring = "vakarieniauti";
+console.log(mystring.charAt(5));
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+console.log(array3);
+const str1 = "Cats are the best!";
+
+console.log(str1.endsWith("best!"));
+// Expected output: true
+
+console.log(str1.endsWith("best", 17));
+// Expected output: true
+
+const str2 = "Is this a question?";
+
+console.log(str2.endsWith("question"));
+
+// const nekeiciamas, objektai parasomi
+// let keiciamim, pagrinde let naudojam kintamiems
