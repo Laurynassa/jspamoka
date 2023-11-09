04757f8acb188aefa55290916f8ca30676cda692
Moksleivis <Moksleivis@kitm.local>
2023-11-09 10:30:03 +0200

    dadeta

--- script.js
+++ script.js
@@ -35,17 +35,97 @@ const array1 = ["a", "b", "c"];
 const array2 = ["d", "e", "f"];
 const array3 = array1.concat(array2);
 console.log(array3);
-const str1 = "Cats are the best!";
 
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.concat(text2, text3).charAt(16);
+// console.log(concatenatedText);
+
+const str1 = "Cats are the best!";
 console.log(str1.endsWith("best!"));
 // Expected output: true
-
 console.log(str1.endsWith("best", 17));
 // Expected output: true
-
 const str2 = "Is this a question?";
-
 console.log(str2.endsWith("question"));
 
+const beasts = ["ant", "bison", "camel", "duck", "bison"];
+
+console.log(beasts.indexOf("bison"));
+// Expected output: 1
+
+// Start from index 2
+console.log(beasts.indexOf("bison", 2));
+// Expected output: 4
+
+console.log(beasts.indexOf("giraffe"));
+// Expected output: -1
+// let text4 = "laba diena.kaip sekasi?Grazi diena";
+// let concatenatedText1 = text1.length();
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.repeat(3);
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.replace("diena", "ryta");
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.search("ie");
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.slice(3, 6);
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.split("");
+// console.log(concatenatedText);
+
+// let text1 = "laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.substring(5, 9);
+// let toUpper = text1.charAt(5).toUpperCase();
+// let subs = text1.substring(6, 10);
+// let concanted = toUpper.concat(subs);
+// console.log(concatenatedText);
+// console.log(toUpper);
+// console.log(subs);
+// console.log(concanted);
+
+// let text1 = true;
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.toString();
+// console.log(typeof concatenatedText);
+
+// let text1 = "    laba diena.";
+// let text2 = "kaip sekasi?";
+// let text3 = "Grazi diena";
+// let concatenatedText = text1.trim();
+// console.log(concatenatedText);
+
+// let x = 6;
+// console.log(++x);
+// console.log(x--);
+// console.log(x);
+
+let tekstas = "labas";
+tekstas = "ate";
+console.log(tekstas);
 // const nekeiciamas, objektai parasomi
 // let keiciamim, pagrinde let naudojam kintamiems
